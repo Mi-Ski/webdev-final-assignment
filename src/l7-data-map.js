1d3bce942d902be0e781353f293cbcd7b7bcc2da
MiSki <skibamichal@interia.pl>
2021-12-11 01:36:45 +0100

    code splitting

---
+++ src/l7-data-map.js
@@ -0,0 +1,50 @@
+import * as L7 from '@antv/l7';
+
+const mapScene = new L7.Scene({
+	id: 'map',
+	map: new L7.Mapbox({
+		style: 'mapbox://styles/michalskiba/ckwpk7z3d5ia815pbwnki3hrj',
+		center: [120.19382669582967, 30.258134],
+		pitch: 40,
+		zoom: 8,
+		token:
+			'pk.eyJ1IjoibWljaGFsc2tpYmEiLCJhIjoiY2t3cGs1OGVqMGRvMTJ3cW9hMTBzYjZtdSJ9.P0xUwkWHMjZCzpWbmjn0nw',
+	}),
+});
+
+
+export default fetch('https://gw.alipayobjects.com/os/rmsportal/oVTMqfzuuRFKiDwhPSFL.json')
+	// fetch('./db.json')
+	.then((res) => res.json())
+	.then((data) => {
+		const pointLayer = new L7.PointLayer({})
+			.source(data.list, {
+				parser: {
+					type: 'json',
+					x: 'j',
+					y: 'w',
+				},
+			})
+			.shape('cylinder')
+			.size('t', function (level) {
+				return [3, 2, level * 2 + 20];
+			})
+			.color('t', [
+				'#094D4A',
+				'#146968',
+				'#1D7F7E',
+				'#289899',
+				'#34B6B7',
+				'#4AC5AF',
+				'#5FD3A6',
+				'#7BE39E',
+				'#A1EDB8',
+				'#CEF8D6',
+			])
+			.style({
+				opacity: 1.0,
+			});
+		mapScene.addLayer(pointLayer);
+	});
+
+mapScene.map.on('click', (e) => console.log(e));
