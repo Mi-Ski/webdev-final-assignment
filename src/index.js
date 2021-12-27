49bd50bf0a8dde98e9f66e47ac338b53bcea32cd
MiSki <skibamichal@interia.pl>
2021-12-27 04:51:31 +0100

    media queries pt.2

--- src/index.js
+++ src/index.js
@@ -1,38 +1,45 @@
 import './styles/main.scss';
 import 'remixicon/fonts/remixicon.css';
 import headerInit from './threejs-header.js';
-import './l7-data-map.js';
+import mapInit from './l7-data-map.js';
 
 const logoDark = require('./images/logo-dark.svg');
 const logoLight = require('./images/logo-light.svg');
 const dmSwitch = document.querySelector('#darkmodeSwitch');
-const sidebarSVGlogo = document.querySelector('.sidebar__svg'); 
+const sidebarSVGlogo = document.querySelector('.sidebar__svg');
 
 window.onload = () => {
 	dmSwitch.checked = true;
-} 
+};
 
-document.querySelector('.button__show-sidebar').addEventListener('click', () => {
-	document.querySelector('.sidebar').classList.toggle('sidebar--visible');
-})
+document
+	.querySelector('.button__show-sidebar')
+	.addEventListener('click', () => {
+		document.querySelector('.sidebar').classList.toggle('sidebar--visible');
+	});
 
 // darkmode button handler
-dmSwitch.addEventListener('change', (e) => {
+let darkMode = true;
 
-	document.querySelectorAll('.landing-logo').forEach(el => {
+dmSwitch.addEventListener('change', (e) => {
+	document.querySelectorAll('.landing-logo').forEach((el) => {
 		el.classList.toggle('invert');
-	})
+	});
 
 	if (e.target.checked) {
 		document.body.setAttribute('data-theme', 'dark');
 		sidebarSVGlogo.src = logoLight;
+		darkMode = true;
 	} else {
 		document.body.removeAttribute('data-theme');
 		sidebarSVGlogo.src = logoDark;
+		darkMode = false;
 	}
 
 	headerInit();
+	mapInit(darkMode);
 });
 
 // three js header init
 headerInit();
+mapInit(darkMode);
--- src/l7-data-map.js
+++ src/l7-data-map.js
@@ -1,12 +1,16 @@
 import * as L7 from '@antv/l7';
+import { MapServiceEvent } from '@antv/l7';
+
+const lightMap = 'mapbox://styles/michalskiba/ckxnqlo3ycd4j14pbznf9y047';
+const darkMap = 'mapbox://styles/michalskiba/ckwpk7z3d5ia815pbwnki3hrj';
 
 const mapScene = new L7.Scene({
 	id: 'map',
 	map: new L7.Mapbox({
 		style: 'mapbox://styles/michalskiba/ckwpk7z3d5ia815pbwnki3hrj',
-		center: [120.19382669582967, 30.258134],
+		center: [110.19382669582967, 30.258134],
 		pitch: 40,
-		// zoom: 5,
+		zoom: 2.5,
 		maxZoom: 8,
 		minZoom: 3,
 		token:
@@ -14,58 +18,68 @@ const mapScene = new L7.Scene({
 	}),
 });
 
+export default mapInit = (darkTheme) => {
+	fetch('https://gw.alipayobjects.com/os/rmsportal/oVTMqfzuuRFKiDwhPSFL.json')
+		// fetch('./db.json')
+		.then((res) => res.json())
+		.then((data) => {
+			const pointLayer = new L7.PointLayer({})
+				.source(data.list, {
+					parser: {
+						type: 'json',
+						x: 'j',
+						y: 'w',
+					},
+				})
+				.shape('cylinder')
+				.size('t', function (level) {
+					return [3, 2, level * 2 ];
+				})
+				.color('t', [
+					'#fde7e9C',
+					'#fccfd3',
+					'#fab7bd',
+					'#f8a0a7',
+					'#f5707b',
+					'#f78891',
+					'#f45b69',
+					'#f35865',
+					'#f2404f',
+					'#f02839',
+				])
+				.style({
+					opacity: 1.0,
+				});
 
-export default fetch('https://gw.alipayobjects.com/os/rmsportal/oVTMqfzuuRFKiDwhPSFL.json')
-	// fetch('./db.json')
-	.then((res) => res.json())
-	.then((data) => {
+			console.log(mapScene.getLayers());
+			mapScene.removeAllLayer();
+			mapScene.addLayer(pointLayer);
 
-		const pointLayer = new L7.PointLayer({})
-			.source(data.list, {
-				parser: {
-					type: 'json',
-					x: 'j',
-					y: 'w',
-				},
-			})
-			.shape('cylinder')
-			.size('t', function (level) {
-				return [3, 2, level * 2 + 12];
-			})
-			.color('t', [
-				'#094D4A',
-				'#146968',
-				'#1D7F7E',
-				'#289899',
-				'#34B6B7',
-				'#4AC5AF',
-				'#5FD3A6',
-				'#7BE39E',
-				'#A1EDB8',
-				'#CEF8D6',
-			])
-			.style({
-				opacity: 1.0,
-			});
+			if (darkTheme) {
+			mapScene.setMapStyle(darkMap);
+			} else {
+			mapScene.setMapStyle(lightMap);
+			}
+		});
 
-		mapScene.addLayer(pointLayer);
+	// prevent scrollwheel zoom without ctrl or alt pressed
+	mapScene.map.on('wheel', (event) => {
+		if (event.originalEvent.ctrlKey) {
+			return;
+		}
+
+		if (event.originalEvent.metaKey) {
+			return;
+		}
+
+		if (event.originalEvent.altKey) {
+			return;
+		}
+
+		event.preventDefault();
 	});
 
-	// prevent scrollwheel zoom without ctrl or alt pressed 
-	mapScene.map.on("wheel", event => {
-    if (event.originalEvent.ctrlKey) {
-        return;
-    }
+	console.log(':D');
+};
 
-    if (event.originalEvent.metaKey) {
-        return;
-    }
-
-    if (event.originalEvent.altKey) {
-        return;
-    }
-
-    event.preventDefault();
-});
-
-mapScene.map.on('click', (e) => console.log(e));
+// mapScene.map.on('click', (e) => console.log(e));
